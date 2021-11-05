59609b88fde8b836f556193ac54268fc309a53ff
webert maciel <webert_maciel@hotmail.com>
2021-11-04 21:44:06 -0300

    two commit

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const bodyParser = require('body-parser');
 const PORT = 5000
+
+let consoleBody = (req, res, next) => {
+    console.log(req.body)
+    next()
+}
+
+let mid = (req, res) => {
+    res.send("hello world!");
+    // res.set("Content-Type", "text/plain"); /* para auterar text*/
+    // res.type('html') /* para auter o tipo se e html ou json etc */
+}
 
 // app.use(express.static(path.join(__dirname, 'client')))
 
-app.get("/", (req, res) => {
-    // res.set("Content-Type", "text/plain"); /* para auterar text*/
-    // res.type('html') /* para auter o tipo se e html ou json etc */
-    res.send("hello world from GET ");
-})
+
+app.use("/", bodyParser.json())
+app.use("/", consoleBody)
+app.get("/", mid)
+app.post("/", mid)
+
 
 app.listen(PORT, () => {
     console.log(`Server Running on Port:${PORT}`)
